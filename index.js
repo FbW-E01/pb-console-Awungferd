d7f620c16d30d1408855912801b864d02d05cd83
Ferdinand <ndimbecha@gmail.com>
2021-03-29 14:15:39 +0200

    Introduction to JS

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const birth = "1984";
 const city = "Hamburg"
 const job = "Teacher";
 
-const hobbies = "dancing, poetry, cycling gazing the stars";
+const hobbies = "dancing, poetry, cycling, star gazing";
 console.log(hisName,  "was born in", birth,". In the city of",  city, ". He is a",  job,  ". And in his spare time he loves", hobbies);
