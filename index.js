dd0eb2cc0c63299d6496377d5858376aa57f29e8
Ferdinand <ndimbecha@gmail.com>
2021-03-29 14:09:45 +0200

    write simple  and commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log = "This is an exercise in console logging";
+console.log("This is an exercise in console logging");
+const instruct = "This is the value.";
+console.log(instruct)
+const firstName = "Ferdinand"
+const lastName = "Mbecha"
+
+console.log(firstName, lastName)
+// someone's profile
+const hisName = "John Bull";
+const birth = "1984";
+const city = "Hamburg"
+const job = "Teacher";
+
+const hobbies = "dancing, poetry, cycling gazing the stars";
+console.log(hisName,  "was born in", birth,". In the city of",  city, ". He is a",  job,  ". And in his spare time he loves", hobbies);
